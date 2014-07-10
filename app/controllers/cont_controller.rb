5ba8aa7864c42fdfb62a64d780cc2bc10cff62e5
menna Eid <menna.ali.eid@gmail.com>
2014-07-10 11:16:32 +0300

    msg

---
+++ app/controllers/cont_controller.rb
@@ -0,0 +1,7 @@
+class ContController < ApplicationController
+  def home
+  end
+
+  def help
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'cont/help'
+
   get 'cont/home'
 
   get 'cont/about'
