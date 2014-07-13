5f3d7a290ade7064ff4539d7dc69a406287de84a
menna Eid <menna.ali.eid@gmail.com>
2014-07-13 13:01:13 +0300

    msg

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,9 @@
 module  ApplicationHelper 
 
   #  Returns the  full title  on a per-page  basis. 
-  def  full_title (page_title) 
+  def  full_title(page_title) 
     base_title   = "Demo" 
-     if page_title .empty? 
+     if page_title.empty? 
        base_title 
     else 
        "#{base_title }  |  #{page_title }" 
