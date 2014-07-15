6cab464fe0dbfbd70a9ab9a3a14c76dc9f170abf
menna Eid <menna.ali.eid@gmail.com>
2014-07-15 13:45:17 +0300

    msg

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,25 @@
 class User < ActiveRecord::Base
-	attr_accessible :name, :email
-
+	#attr_accessible
 	validates :name, presence: true, length: { maximum: 40 }  # it means that the name should be entered and the length of the name
 	#should not be more than 40 charachters ,where presence has  a hasher it  which is what aft and length also takes a hash 
 	EMAIL_CORRECT_FORMAT = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/ #starting from \A is te beginning of the string the contains the correct format of an email
-	
+	has_secure_password #it create sthe library has_secure_password w we add it in the gem file and creates password and password_confirmation
 
 
     before_save { |user| user.email = user.email.downcase}
-	validates :email, presence: true, format: { with: EMAIL_CORRECT_FORMAT }, uniqueness: { case_sensitive: false } #uniqueness is true
-	#but with no case sensitivity?????????????????????????????????????????
+     before_validation do |user| 
+       user.name = "random" if user.name.blank? 
+  	  end
 
-	validates :password, presence: true, length: { maximum: 8 }
+
+	validates :email, presence: true, format: { with: EMAIL_CORRECT_FORMAT }, uniqueness: { case_sensitive: false } #uniqueness is false (Menna@hotmail.com is the same as menna@hotmail.com)
+
+	validates :password, presence: true, length: { minimum: 8 }
 
 	validates :password_confirmation, presence: true
+
+	def name_longer_than_eight
+		User.where("length(name) > 8")
+	end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140714194020) do
+ActiveRecord::Schema.define(version: 20140715083333) do
 
   create_table "authors", force: true do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(version: 20140714194020) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
